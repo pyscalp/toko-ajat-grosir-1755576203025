c12b9e4b50007fb3a6ea00bdf6eb4b2cf2caafe0
pyscalp <pyscalp@gmail.com>
2025-08-19 11:03:44 +0700

    Add src/App.jsx - Complete Vite project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import GalleryPage from './pages/GalleryPage';
+import ContactPage from './pages/ContactPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </main>
+      <Footer />
+      <Toaster position="bottom-right" />
+    </BrowserRouter>
+  );
+}
+
+export default App;
